f85fbff4f541ee8d6b47a8dfeea86d7d8499321c
maximuusS <ksaurav0211@gmail.com>
2021-12-27 15:26:33 +0530

    Added unhandled promise rejection

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,15 @@ dotenv.config({ path: 'backend/config/config.env'})
 //connecting to database
 connectDB();
 
-app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT, () => {
     console.log(`Server started on Port: ${process.env.PORT}`)
+})
+
+//Handling unhandled promise rejections
+process.on('unhandledRejection', err => {
+    console.log(`Error: ${err.message}`);
+    console.log('Server is being shutdown due to unhandled promise rejection');
+    server.close(() => {
+        process.exit(1);
+    })
 })
