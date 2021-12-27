067226e37cb9ab8ab2d16dbf4187de3c8bbe81a7
maximuusS <ksaurav0211@gmail.com>
2021-12-27 15:20:10 +0530

    Added email and token utilities

---
+++ backend/utils/errorHandler.js
@@ -0,0 +1,15 @@
+//Class error handler
+
+class ErrorHandler extends Error {
+    constructor(message, statusCode) {
+
+        super(message);
+        this.statusCode = statusCode;
+ 
+
+        //create stack property for object
+        Error.captureStackTrace(this, this.constructor)
+    }
+}
+
+module.exports = ErrorHandler;
---
+++ backend/utils/jwtToken.js
@@ -0,0 +1,23 @@
+//Create and send token     
+
+const sendToken = (user, statusCode, res) => {
+
+    //Creating token
+    const token = user.getJwtToken();
+
+    //Options for cookie
+    const options = {
+        expires: new Date(
+            Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000
+        ),
+        httpOnly: true
+    }
+    
+    res.status(statusCode).cookie('token', token, options).json({
+        success: true,
+        user,
+        token
+    })
+}
+
+module.exports = sendToken;
