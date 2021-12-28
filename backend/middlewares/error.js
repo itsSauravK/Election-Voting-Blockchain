a0c71d73a6e431409fe9eff58133caf26a9d87d2
maximuusS <ksaurav0211@gmail.com>
2021-12-28 14:46:46 +0530

    Added resister user function

--- backend/middlewares/error.js
+++ backend/middlewares/error.js
@@ -41,8 +41,7 @@ module.exports = (err, req, res, next ) => {
         res.status(err.statusCode).json({
             success: false,
             error: err,
-            errMessage: err.message,
-            stack: err.stack    
+            errMessage: err.message,   
         })
 
     
