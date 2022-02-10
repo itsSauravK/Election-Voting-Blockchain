953a106b1ed88706b0913eefba39e3c0f250d4a2
maximuusS <ksaurav0211@gmail.com>
2022-02-10 16:24:36 +0530

    Fixed and removed redundant election routes

--- backend/controllers/electionControllers.js
+++ backend/controllers/electionControllers.js
@@ -5,24 +5,6 @@ const User = require('../models/user');
 const sendEmail = require('../utils/sendEmail');
 
 
-// Function to add election one election in db 
-//=> /api/election/addElection    
-//- can only be accessed by admin
-exports.addElection = catchAsyncError(async (req, res, next) => {
-
-    const { name, description } = req.body;
-
-    const election = await Election.create({
-        name,
-        description
-    })
-    res.status(200).json({
-        success:true,
-        election
-    })
-
-} )
-
 //Function to start election 
 // => api/election/startElection
 // admin access
@@ -46,115 +28,60 @@ exports.startElection = catchAsyncError(async (req, res, next) => {
 })
 
 
-//Function to show current election
-// => api/electron/showElection
-//user and admin access
-
-exports.showCurrentElection = catchAsyncError(async (req, res, next) => {
-
-    const election = await Election.find({isOngoing: true});
-    if(!election){
-        res.status(200).json({
-            success:false,
-            message:"No ongoing election"
-        })
-    }
-    res.status(200).json({
-        success:true,
-        election
-    })
-})
-
 //Function to end election
 // => api/election/endElection
 // admin access
 exports.endElection = catchAsyncError(async (req, res, next) => {
 
-    let { candidates } = req.body;
+    let { address } = req.body;
 
-    const election = await Election.findOne({isOngoing:true});
-
-    if(!election){
-        return next(new ErrorHandler('No ongoing elections', 401));
-    }
     //make election ongoing false in user model
     const users = await User.find();
-    let isDraw = false;
+    // let isDraw = false;
     users.forEach(user => {
         
         //sending an email to each user that election has ended
-        electionEmail(user, `Election has ended. Visit http://localhost:4000/api/election/${election.id}`, next);
+        electionEmail(user, `Election has ended. Visit http://localhost:3000/election/${address}`, next);
 
         //Updating each user ongoing variable to false
         updateUser(user, false);
+
+        //updating each user has voted to false
+        updateUserVote(user);
     });
 
     //sort canddiates
 
-    candidates = sortCandidate(candidates, next);
+    // candidates = sortCandidate(candidates, next);
 
     //checking if there is a draw 
 
-    if(candidates[candidates.length - 2].votes == candidates[candidates.length - 1].votes){
-        isDraw = true;
+    // if(candidates[candidates.length - 2].votes == candidates[candidates.length - 1].votes){
+    //     isDraw = true;
         
-    }
+    // }
 
     //update in datbase 
 
-    const newData = {
-        isOngoing: false,
-        candidates : candidates,
-        isDraw: isDraw
-    }
+    // const newData = {
+    //     isOngoing: false,
+    //     candidates : candidates,
+    //     isDraw: isDraw
+    // }
     
-    const updatedElection = await Election.findByIdAndUpdate(election.id, newData,{
+    // const updatedElection = await Election.findByIdAndUpdate(election.id, newData,{
         
-        new: true,
-        runValidators: true,
-        useFindAndModify: false
-    });
+    //     new: true,
+    //     runValidators: true,
+    //     useFindAndModify: false
+    // });
 
     res.status(200).json({
         success:true,
-        updatedElection
     })
 
 })
 
-//function to show one election result
-///api/election/:id    
-//- can be accessed by anyone
-exports.oneResult = catchAsyncError(async (req, res, next) => {
-
-    election = await Election.findById(req.params.id);
-
-    if(!election){
-        return next(new ErrorHandler('Invalid ID', 401));
-    }
-    res.status(200).json({
-        success: true,
-        election
-    })
-})
-//function to show all elections result in db 
-//=> /api/election/allElections   
-//- Can be accessed by anyone
-exports.showAllElection = catchAsyncError(async (req, res, next) => {
-
-    const elections = await Election.find({isOngoing:false});
-
-    if(!elections){
-        res.status(200).json({
-            success: true,
-            message: "No ongoing elections"
-        })
-    }
-    res.status(200).json({
-        success: true,
-        elections
-    })
-})
 
 //function to chcnage user onGoing
 async function updateUser(user, bool){
@@ -165,6 +92,16 @@ async function updateUser(user, bool){
             useFindAndModify: false
         })
 }
+
+//function to change user hasVoted
+async function updateUserVote(user){
+    await User.findByIdAndUpdate(user.id, {hasVoted: false},{
+            new: true,
+            runValidators: true,
+            useFindAndModify: false
+    })
+}
+
 //function to send email that election has started
 async function electionEmail(user, message ,next){
     
--- backend/routes/election.js
+++ backend/routes/election.js
@@ -7,26 +7,18 @@ const {
 } = require('../middlewares/auth');
 
 const {
-    addElection,
-    showCurrentElection, 
     startElection,
-    showAllElection,
-    oneResult,
     endElection
 } = require('../controllers/electionControllers');
 
 
 
-//user routes
-router.route('/showElection').get(isAuthenticatedUser, showCurrentElection);
+
 
 //admin routes
-router.route('/addElection').post(isAuthenticatedUser, authorizeRoles('admin'), addElection);
 router.route('/startElection').get(isAuthenticatedUser, authorizeRoles('admin'), startElection);
 router.route('/endElection').put(isAuthenticatedUser, authorizeRoles('admin'), endElection);
 
-//accesed by all routes
-router.route('/AllElections').get(showAllElection);  //result of all elections
-router.route('/result/:id').get(oneResult); //result of one election
+
 
 module.exports = router;
