d5af5f98e3bb271ca421b7862023543956252d0d
maximuusS <ksaurav0211@gmail.com>
2022-02-13 16:52:49 +0530

    Added logout functionality

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,10 +1,22 @@
-/* prettier-ignore */
-import { useContext } from "react";
+import axios from 'axios';
+import { useContext } from 'react';
 
-import { Link } from "react-router-dom";
-import AuthContext from "../store/auth-context";
+import { Link } from 'react-router-dom';
+import AuthContext from '../store/auth-context';
 const SideBar = () => {
-   const { user } = useContext(AuthContext);
+   const { user, setUser, notify } = useContext(AuthContext);
+
+   const logoutHandler = async () => {
+      try {
+         await axios.get('http://localhost:4000/api/election/logout', {
+            withCredentials: true,
+         });
+         setUser(null);
+         notify('User Logged out', 'success');
+      } catch (err) {
+         notify(err.response.data.errMessage, 'error');
+      }
+   };
    return (
       <div className='top-0 right-0 fixed'>
          <h2 className='text-center'>This is the sidebar</h2>
@@ -16,6 +28,9 @@ const SideBar = () => {
             )}
             <li>
                <Link to='/ '>Landing page</Link>
+            </li>
+            <li>
+               <button onClick={logoutHandler}>Logout</button>
             </li>
             <li>
                <Link to='/login '>Login</Link>
--- frontend/src/pages/LandingPage/LandinPage.js
+++ frontend/src/pages/LandingPage/LandinPage.js
@@ -1,54 +1,54 @@
 /**
  * @prettier
  */
-import { useContext, useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import { useNavigate } from "react-router";
-import AuthContext from "../../store/auth-context";
-import Loading from "../../components/Loading";
-import { useEndElection } from "../../components/hooks/end-election";
+import { useContext, useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router';
+import AuthContext from '../../store/auth-context';
+import Loading from '../../components/Loading';
+import { useEndElection } from '../../components/hooks/end-election';
 
 const LandingPage = () => {
    const [loading, setLoading] = useState(false);
    const { user, notify, election } = useContext(AuthContext);
-   useEffect(() => {
-      if (!user) {
-         notify("Please login first", "error");
-         //navigate('/login');
-      }
-   }, [user]);
+   // useEffect(() => {
+   //    if (!user) {
+   //       notify("Please login first", "error");
+   //       //navigate('/login');
+   //    }
+   // }, [user]);
 
    //use effect to remove bug where user rejects second transaction while ending election
-   useEndElection("", setLoading);
+   useEndElection('', setLoading);
 
    return (
       <>
          {/* No user */}
          {!loading && !user && (
-            <Link to="/login">
+            <Link to='/login'>
                <button>Login</button>
             </Link>
          )}
 
          {/*User and election ongoing */}
          {!loading && user && user.electionOngoing && (
-            <Link to="/election">
+            <Link to='/election'>
                <button>Vote now</button>
             </Link>
          )}
 
          {/* User role and no election ongoing */}
-         {!loading && user && user.role === "user" && !user.electionOngoing && (
+         {!loading && user && user.role === 'user' && !user.electionOngoing && (
             <p>No election ongoing</p>
          )}
 
          {/* Admin role and no election ongoing */}
          {!loading &&
             user &&
-            user.role === "admin" &&
+            user.role === 'admin' &&
             !user.electionOngoing &&
-            election === "0x0000000000000000000000000000000000000000" && (
-               <Link to="/addElection">
+            election === '0x0000000000000000000000000000000000000000' && (
+               <Link to='/addElection'>
                   <button>Start Election</button>
                </Link>
             )}
@@ -56,10 +56,10 @@ const LandingPage = () => {
          {/*Admin role and added an election but did not start it*/}
          {!loading &&
             user &&
-            user.role === "admin" &&
+            user.role === 'admin' &&
             !user.electionOngoing &&
-            election !== "0x0000000000000000000000000000000000000000" && (
-               <Link to="/election">
+            election !== '0x0000000000000000000000000000000000000000' && (
+               <Link to='/election'>
                   <button>Go to Election</button>
                </Link>
             )}
