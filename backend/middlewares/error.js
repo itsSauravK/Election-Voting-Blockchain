4a1f1bd06cdf1898dab6c6d3f3f771bc8a283f7e
maximuusS <ksaurav0211@gmail.com>
2022-02-15 21:50:00 +0530

    Fixed error handling in backend

--- backend/middlewares/error.js
+++ backend/middlewares/error.js
@@ -1,4 +1,4 @@
-const ErrorHandler = require("../utils/errorHandler.js");
+const ErrorHandler = require('../utils/errorHandler.js');
 
 module.exports = (err, req, res, next) => {
    let statusCode = err.statusCode;
@@ -6,41 +6,41 @@ module.exports = (err, req, res, next) => {
 
    let error = { ...err };
 
-   error.message = err.message || "Internal server error";
+   error.message = err.message || 'Internal server error';
 
    // Wrong mongoose object id error
-   if (err.name === "CastError") {
+   if (err.name === 'CastError') {
       const message = `Resource not found. Invalid: ${err.path}`;
       error = new ErrorHandler(message, 400);
    }
 
    //Handling validation error
-   if (err.name === "ValidationError") {
+   if (err.name === 'ValidationError') {
       const message = Object.values(err.errors).map((value) => value.message);
       error = new ErrorHandler(message, 400);
    }
 
-   // //Handling duplicate key error. Registering with same email
-   // if(err.code = 11000) {
-   //     const message = `Duplicate ${Object.keys(err.keyValue)} entered`;
-   //     error = new ErrorHandler(message, 400);
-   // }
+   //Handling duplicate key error. Registering with same email
+   if (err.code == 11000) {
+      const message = `Duplicate ${Object.keys(err.keyValue)} entered`;
+      error = new ErrorHandler(message, 400);
+   }
 
    //Handling wrong JWT token
-   if (err.name === "JsonWebTokenError") {
-      const message = "JSON Web Token is invalid. Try again";
+   if (err.name === 'JsonWebTokenError') {
+      const message = 'JSON Web Token is invalid. Try again';
       error = new ErrorHandler(message, 400);
    }
 
    //Expired JWT Token
-   if (err.name === "TokenExpiredError") {
-      const message = "JSON Web Token is expired. Try again";
+   if (err.name === 'TokenExpiredError') {
+      const message = 'JSON Web Token is expired. Try again';
       error = new ErrorHandler(message, 400);
    }
 
    res.status(statusCode).json({
       success: false,
-      error: err,
-      errMessage: err.message,
+      error: error,
+      errMessage: error.message,
    });
 };
