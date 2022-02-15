c7a1b3140f1b9763fca4ae64f73898fd14624a80
maximuusS <ksaurav0211@gmail.com>
2022-02-15 21:28:55 +0530

    added authorization for add user page

--- frontend/src/pages/AddUser/AddUser.js
+++ frontend/src/pages/AddUser/AddUser.js
@@ -1,4 +1,19 @@
+import { useEffect, useContext } from 'react';
+
 const AddUser = () => {
+   const { user, notify } = useContext(AuthContext);
+   const [loading, setLoading] = useState(false);
+   useEffect(() => {
+      if (!user) {
+         notify('You need to login first', 'error');
+         navigate('/login');
+      } else {
+         if (user.role === 'user') {
+            notify('You do not have access to this route', 'error');
+            navigate('/');
+         }
+      }
+   });
    return (
       <>
          <p>Add user</p>
