1efe2e67e1169223540d12088914c5c18d42c67e
maximuusS <ksaurav0211@gmail.com>
2022-02-15 21:53:24 +0530

    Added form to register user:

--- frontend/src/pages/AddUser/AddUser.js
+++ frontend/src/pages/AddUser/AddUser.js
@@ -1,8 +1,15 @@
-import { useEffect, useContext } from 'react';
+import { useEffect, useContext, useState } from 'react';
+import { useNavigate } from 'react-router';
+import AuthContext from '../../store/auth-context';
 
 const AddUser = () => {
    const { user, notify } = useContext(AuthContext);
    const [loading, setLoading] = useState(false);
+   const navigate = useNavigate();
+   const [name, setName] = useState('');
+   const [email, setEmail] = useState('');
+   const [eAddress, setEAddress] = useState('');
+
    useEffect(() => {
       if (!user) {
          notify('You need to login first', 'error');
@@ -14,9 +21,33 @@ const AddUser = () => {
          }
       }
    });
+
+   const userRegisterHandler = (e) => {
+      e.preventDefault();
+      console.log('click');
+   };
    return (
       <>
          <p>Add user</p>
+         <form onSubmit={userRegisterHandler}>
+            <label>Name</label>
+            <input value={name} onChange={(e) => setName(e.target.value)} />
+            <label>Email</label>
+            <input
+               value={email}
+               onChange={(e) => {
+                  setEmail(e.target.value);
+               }}
+            />
+            <label>Ethereum Address</label>
+            <input
+               value={eAddress}
+               onChange={(e) => {
+                  setEAddress(e.target.value);
+               }}
+            />
+            <button type='submit'>Register</button>
+         </form>
       </>
    );
 };
