56011c2595f120b6842bdea4ae65bce6a50a2dc0
Irfan Shaikh <irfansk.se@gmail.com>
2022-03-02 23:55:00 +0530

    connect css file to index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import "./index.css";
 import { AuthContextProvider } from './store/auth-context';
 
 ReactDOM.render(
